7a38d63289e12c3bad83d3c42abeb15ba680069a
Ayush <ayushsinghzzzzz38@gmail.com>
2025-09-16 16:57:18 +0530

    coliv update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'owners-hub',
+  appName: 'CoLiv-Manager',
   webDir: 'dist'
 };
 
