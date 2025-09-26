e7dd0e59db7909f2fc06ff203306c70ab028b714
Ayush <ayushsinghzzzzz38@gmail.com>
2025-09-26 10:26:31 +0530

    debugging p1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'CoLiv.Manager',
+  appId: 'com.coliv.manager',
   appName: 'CoHub - Property Management',
   webDir: 'dist'
 };
